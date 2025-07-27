23dcf4d3da83411d2c84b4b084b97583d9c08a7e
GiselleBarbosa <g88barbosa@gmail.com>
2025-07-27 13:06:23 -0300

    aula29: implementação dos formulários de cadastro e login

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  signupForm = this.formBuilder.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLogin() {
+    console.log('Login Dados: ', this.loginForm.value);
+  }
+
+  onSubmitSignup() {
+    console.log('Cadastro Dados: ', this.signupForm.value);
+  }
 }
