eceec61732354e9f4fa6bbb7c1cbafe3b8b3ca99
GiselleBarbosa <g88barbosa@gmail.com>
2025-11-08 15:53:14 -0300

    feat: implementando consumo da API de produtos no dashboard component

--- frontend/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ frontend/src/app/modules/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Severity } from 'src/app/enum/severity.enum';
+import { Products } from 'src/app/interfaces/products-interface';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ToastMessagesService } from 'src/app/services/toast-messages/toast-messages.service';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -6,6 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: [],
   standalone: false
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  productsList: Products.ProductsResponse[] = [];
+
+  constructor(private productsService: ProductsService, private toastMessageService: ToastMessagesService) { }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productsService
+      .getAllProducts()
+      .subscribe({
+        next: response => {
+          if (response.length > 0) {
+            this.productsList = response;
+            console.log("Products: ", response);
+          }
+        },
+        error: (error) => {
+          setTimeout(() => {
+            this.toastMessageService.show(
+              Severity.ERROR,
+              "Erro inesperado",
+              "Não foi possível exibir os produtos.",
+              3500);
+          }, 2000);
+
+          console.error("Erro inesperado: ", error);
+        }
+      });
+  }
 
 }
